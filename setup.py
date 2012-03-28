624033b04d281979e80fc38f600200341501c8e8
abdul <abdul@mongolab.com>
2012-03-27 19:20:11 -0700

    revert last change

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ else:
     features = {"c-ext": c_ext}
 
 setup(
-    name="zzz-dampier-pymongo",
+    name="dampier-pymongo",
     version="2.1.1",
     description="Python driver for MongoDB <http://www.mongodb.org>",
     long_description=readme_content,
